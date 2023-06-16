5456357e0e87cb09b19b960cd3931bef8f956763
RGR <rg.robingranger@gmail.com>
2023-06-16 16:14:33 +0200

    tu(express): add tests for validation function

--- express/src/lib/movements/validate-movements.test.js
+++ express/src/lib/movements/validate-movements.test.js
@@ -30,8 +30,8 @@ test('it should return an error when there are insatisfied balances', () => {
         { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -200 },
         { id: 22, date: new Date(1995, 3, 25).getTime(), amount: 50 },
         { id: 68, date: new Date(2016, 7, 25).getTime(), amount: -50 },
-        { id: 68, date: new Date(2017, 7, 25).getTime(), amount: 500 },
-        { id: 68, date: new Date(2022, 7, 25).getTime(), amount: -50 },
+        { id: 30, date: new Date(2017, 7, 25).getTime(), amount: 500 },
+        { id: 27, date: new Date(2022, 7, 25).getTime(), amount: -50 },
     ]
     const balances = [
         { date: new Date(2018, 8, 26).getTime(), balance: 100 },
@@ -45,5 +45,67 @@ test('it should return an error when there are insatisfied balances', () => {
             `Balance with Date "${new Date(2018, 8, 26).getTime()}" is not satisfied. Check if there are missing movements.`,
             `Balance with Date "${new Date(2024, 8, 26).getTime()}" is not satisfied. Check if there are missing movements.`
         ]
+    })
+})
+
+test('it should return a success when all the balances are satisfied (simple)', () => {
+    const movements = [
+        { id: 12, date: new Date(1995, 5, 25).getTime(), amount: 100 },
+        { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -200 },
+    ]
+    const balances = [
+        { date: new Date(2024, 8, 26).getTime(), balance: -100 },
+    ]
+
+    expect(validateMovements(movements, balances)).toStrictEqual({
+        type: 'Success',
+        reasons: []
+    })
+})
+
+test('it should return a success when all the balances are satisfied (multiple)', () => {
+    const movements = [
+        { id: 12, date: new Date(1995, 5, 25).getTime(), amount: 100 },
+        { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -200 },
+        { id: 22, date: new Date(1995, 3, 25).getTime(), amount: 50 },
+        { id: 43, date: new Date(2016, 7, 25).getTime(), amount: -50 },
+        { id: 68, date: new Date(2017, 7, 25).getTime(), amount: 500 },
+        { id: 21, date: new Date(2022, 7, 25).getTime(), amount: -50 },
+    ]
+    const balances = [
+        { date: new Date(2018, 8, 26).getTime(), balance: 450 },
+        { date: new Date(2014, 8, 26).getTime(), balance: 150 },
+        { date: new Date(2024, 8, 26).getTime(), balance: -250 },
+    ]
+
+    expect(validateMovements(movements, balances)).toStrictEqual({
+        type: 'Success',
+        reasons: []
+    })
+})
+
+test('it should return a success when all the balances are satisfied (multiple with duplicates)', () => {
+    const movements = [
+        { id: 12, date: new Date(1995, 5, 25).getTime(), amount: 300 },
+        { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -6000 },
+        { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -6000 },
+        { id: 14, date: new Date(2023, 8, 21).getTime(), amount: -6000 },
+        { id: 22, date: new Date(1995, 3, 25).getTime(), amount: 5000 },
+        { id: 22, date: new Date(1995, 3, 25).getTime(), amount: 5000 },
+        { id: 22, date: new Date(1995, 3, 25).getTime(), amount: 5000 },
+        { id: 43, date: new Date(2016, 7, 25).getTime(), amount: -50 },
+        { id: 68, date: new Date(2017, 7, 25).getTime(), amount: 500 },
+        { id: 21, date: new Date(2022, 7, 25).getTime(), amount: -50 },
+        { id: 21, date: new Date(2022, 7, 25).getTime(), amount: -50 },
+    ]
+    const balances = [
+        { date: new Date(2018, 8, 26).getTime(), balance: 450 },
+        { date: new Date(2014, 8, 26).getTime(), balance: 5300 },
+        { date: new Date(2024, 8, 26).getTime(), balance: -6050 },
+    ]
+
+    expect(validateMovements(movements, balances)).toStrictEqual({
+        type: 'Success',
+        reasons: []
     })
 })
