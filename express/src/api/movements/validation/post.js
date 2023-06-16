1cf4c8d7ce5ae051ca40c785424cba61e1da91a2
RGR <rg.robingranger@gmail.com>
2023-06-16 15:33:42 +0200

    fix(express): move validate function and fix some bugs

--- express/src/api/movements/validation/post.js
+++ express/src/api/movements/validation/post.js
@@ -1,3 +1,5 @@
+import { validateMovements } from "../../../lib/movements/validate-movements"
+
 export function handleRequest(req, res) {
     const parseResult = parseInputs(req.body)
 
@@ -29,46 +31,4 @@ function parseInputs(body) {
             balances: body.balances,
         }
     }
-}
-
-function validateMovements(movements, balances) {
-    if (balances.length === 0) {
-        return {
-            type: 'Error',
-            reasons: ['No balance provided. Unable to validate movements.']
-        }
-    }
-
-    balances.sort((balanceA, balanceB) => balanceA - balanceB)
-
-    const movementsHash = {}
-
-    return balances.reduce((balancesResult, currentBalance, currentBalanceIndex) => {
-        const previousBalance = currentBalanceIndex > 0 ? balances[currentBalanceIndex - 1] : null
-
-        const movementsTotal = movements.reduce((acc, currentMovement) => {
-            // Check if movement is not a duplicate
-            if (movementsHash[movement.id] === undefined) {
-                movementsHash[movement.id] = movement
-
-                // If movement date is between previous and current balance date
-                if (previousBalance !== null || previousBalance.date < movement.date && movement.date <= currentBalance.date) {
-                    acc += currentMovement.amount
-                } else if (movement.date > balances[balances.length - 1].date) {
-                    // if movement date is after the last balance date
-                    balancesResult.type = 'Error'
-                    balancesResult.reasons.push('There are movements after the last balance Date.')
-                }
-            }
-
-            return acc
-        }, 0)
-
-        if (movementsTotal !== currentBalance.balance) {
-            balancesResult.type = 'Error'
-            balancesResult.reasons.push(`Balance number "${currentBalanceIndex + 1}" is not satisfied. Check if there are missing movements.`)
-        }
-
-        return balancesResult
-    }, { type: 'Success', reasons: [] })
 }
