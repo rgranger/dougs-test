e723d520a227881171370bb4bd422d361460d5b2
Robin Granger <robin.granger@analogway.com>
2023-06-18 12:05:53 +0200

    feat(nest): add movement validation api

--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Balance, Movement } from './lib/movements/types';
+import { Result, validateMovements } from './lib/movements/validate-movements';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  validateMovements(movements: Movement[], balances: Balance[]): Result {
+    return validateMovements(movements, balances);
   }
 }
